db8574b5940a0be88e196a28b92bdc425285fdfa
luyunfei.louis <luyunfei.louis@bytedance.com>
2020-03-10 02:01:28 +0800

    try event

--- statEvent.py
+++ statEvent.py
@@ -11,8 +11,8 @@ sns.set()
 sns.set_style('white')
 
 def calcDateDelta(date1, date2):
-	date1 = time.strptime(date1, "%Y%m%d")
-    date2 = time.strptime(date2, "%Y%m%d")
+	date1 = time.strptime(date1, '%Y%m%d')
+    date2 = time.strptime(date2, '%Y%m%d')
 	date1 = datetime.datetime(date1[0],date1[1],date1[2])
     date2 = datetime.datetime(date2[0],date2[1],date2[2])
     return (date2 - date1).days 	
