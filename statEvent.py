c838095ba27cc902ae145a8d878a42bada063bca
luyunfei.louis <luyunfei.louis@bytedance.com>
2020-03-10 02:15:37 +0800

    try event

--- statEvent.py
+++ statEvent.py
@@ -75,7 +75,7 @@ for line in data:
 		for i in range(4):
 			infodic[3][date][i+1] += int(temp[7+i])
 
-print infodic
+print infodic[0]
 for i in range(4):
 	datelist = infodic[i].keys().sort()
 	#print datelist
