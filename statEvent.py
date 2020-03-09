cb85bdecf7b8b0d3c18ff24c28eb447100f180ed
luyunfei.louis <luyunfei.louis@bytedance.com>
2020-03-10 02:02:37 +0800

    try event

--- statEvent.py
+++ statEvent.py
@@ -12,10 +12,10 @@ sns.set_style('white')
 
 def calcDateDelta(date1, date2):
 	date1 = time.strptime(date1, '%Y%m%d')
-    date2 = time.strptime(date2, '%Y%m%d')
+	date2 = time.strptime(date2, '%Y%m%d')
 	date1 = datetime.datetime(date1[0],date1[1],date1[2])
-    date2 = datetime.datetime(date2[0],date2[1],date2[2])
-    return (date2 - date1).days 	
+	date2 = datetime.datetime(date2[0],date2[1],date2[2])
+	return (date2 - date1).days 	
 
 name = ['Numbers', 'Views', 'Likes', 'Comments', 'Shares']
 category = ['All', 'Celebrity', 'Company', 'Government']
