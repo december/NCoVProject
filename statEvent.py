c996f12000a6fdc71764c23b86475e79f803145f
luyunfei.louis <luyunfei.louis@bytedance.com>
2020-03-10 02:05:33 +0800

    try event

--- statEvent.py
+++ statEvent.py
@@ -48,6 +48,8 @@ for line in data:
 	infodic[0][date][0] += 1
 	for i in range(4):
 		infodic[0][date][i+1] += int(temp[7+i])
+	if temp[5] == 'null':
+		continue
 	ct = int(temp[5])
 	cc = int(temp[6])
 	if ct == 0:
