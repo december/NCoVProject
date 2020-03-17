b5d9d93fab0c067caa4f3f52e6acf03f9ed57ab9
luyunfei.louis <luyunfei.louis@bytedance.com>
2020-03-18 01:01:28 +0800

    change file

--- statEvent.py
+++ statEvent.py
@@ -28,8 +28,8 @@ for i in range(6): #all, celebrity, company, government
 provincedic = {} #from date to province to the number of videos, views, likes, comments, shares
 
 path = '../../../data/'
-event = 'tzh'
-dt = '20200315'
+event = 'whfc'
+dt = '20200313'
 fr = open(path+'aweme_event_'+event+'_'+dt+'.text', 'r')
 data = fr.readlines()
 fr.close()
